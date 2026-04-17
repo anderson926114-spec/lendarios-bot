aea3a0224b63a556e4837c7d36d3e2a3b58a1b80
anderson926114-spec <anderson926114@gmail.com>
2026-04-17 09:46:40 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -269,3 +269,15 @@ def teste():
 @app.route("/")
 def home():
     return "BOT ONLINE"
+    @app.route("/teste")
+def teste():
+    try:
+        r = requests.post(
+            "https://hook.us2.make.com/gcgl67hj5uaww80orbjetvjrsuz7ya78",
+            json={"teste": "funcionando"}
+        )
+
+        return f"ENVIADO | STATUS: {r.status_code} | RESPOSTA: {r.text}"
+
+    except Exception as e:
+        return f"ERRO: {str(e)}"
